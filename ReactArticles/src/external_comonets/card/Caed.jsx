7e45f21a274c0a1dc5ef42d28564efde0cdcc0d7
yuvaldmaoz <yuval.maoz2001@gmail.com>
2025-08-13 22:38:39 +0300

    סבבה

--- ReactArticles/src/external_comonets/card/Caed.jsx
+++ ReactArticles/src/external_comonets/card/Caed.jsx
@@ -1,21 +1,23 @@
 import "./Card.css";
+import { Link } from "react-router-dom";
 
 export default function Card({ user, index }) {
   const images = [
     "https://cdn-icons-png.flaticon.com/512/4388/4388193.png",
-
     "https://icon-library.com/images/task-icon/task-icon-0.jpg",
     "https://static.vecteezy.com/system/resources/previews/020/911/740/original/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png",
     "https://static.vecteezy.com/system/resources/previews/020/911/740/original/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png",
   ];
 
+  const routing = ["/orders", "/tasks", "/orders", "/orders"];
+
   const { tables, total_objects } = user;
 
   return (
-    <div className="Card">
-      <img src={images[index]} className="img_card" />
+    <Link to={routing[index]} className="Card">
+      <img src={images[index]} className="img_card" alt={tables} />
       <p className="p_Card">{tables}</p>
       <p className="p_Card">{total_objects}</p>
-    </div>
+    </Link>
   );
 }
